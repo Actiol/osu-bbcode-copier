ab11b4f611a5a394bd1ed6418c187f99e1770164
Actiol <Jonas.s_HansDita17@gmx.de>
2024-11-23 14:38:32 +0100

    added description

--- bbcode.user.js
+++ bbcode.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name        osu! BBCode copier
-// @version     1.0
+// @version     1.1
 // @author      Actiol
 // @match       https://osu.ppy.sh/*
 // @grant       GM_registerMenuCommand
-// @description 11/7/2024, 12:47:18 PM
+// @description Adds a button to me! sections and beatmap descriptions to copy those directly as BBCode
 // @downloadURL https://github.com/Actiol/osu-bbcode-copier/raw/refs/heads/main/bbcode.user.js
 // @updateURL   https://github.com/Actiol/osu-bbcode-copier/raw/refs/heads/main/bbcode.user.js
 // ==/UserScript==
